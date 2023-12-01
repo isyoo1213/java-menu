cabe5a17ae68a1ed873b3efa7965f59b544cf2fa
isyoo1213 <moethns@gmail.com>
2023-12-01 20:10:56 +0900

    feat: ExceptionMessages 오류 케이스 추가 constants/ExceptionMessages - WRONG_NAMES_LENGTH - WRONG_COACHES_AMOUNT

--- src/main/java/menu/constants/ExceptionMessages.java
+++ src/main/java/menu/constants/ExceptionMessages.java
@@ -4,6 +4,8 @@ public enum ExceptionMessages {
     NULL_INPUT("null은 입력할 수 없습니다."),
     EMPTY_INPUT("값을 입력해주세요."),
     WRONG_NAMES_FORMAT("구분자는 쉼표(,)를 사용하고, 이름은 2~4글자로 입력해주세요."),
+    WRONG_NAMES_LENGTH("이름은 2~4글자로 입력해주세요."),
+    WRONG_COACHES_AMOUNT("코치는 2~5명으로 입력해주세요."),
     DUPLICATED_MENUS("중복된 메뉴는 입력할 수 없습니다."),
     NON_EXISTING_MENUS("존재하지 않는 메뉴가 포함되어 있습니다.");
 
