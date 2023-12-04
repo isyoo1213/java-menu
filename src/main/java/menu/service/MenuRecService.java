8dc5db2677b87762f61cb22ae14b4ba80de57ddb
isyoo1213 <moethns@gmail.com>
2023-12-04 16:03:17 +0900

    feat: MenuRecService 코치별 싫어하는 메뉴 저장 구현 service/MenuService - saveCoaches() - saveHateMenus() - saveCoachesInfo() docs/README.md - 기능 명세 구현 체크

---
+++ src/main/java/menu/service/MenuRecService.java
@@ -0,0 +1,32 @@
+package menu.service;
+
+import menu.model.Coach;
+import menu.model.Coaches;
+import menu.model.HateMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuRecService {
+
+    public List<Coach> saveCoaches(List<String> convertedCoachNames) {
+        List<Coach> coaches = new ArrayList<>();
+        for (String convertedCoachName : convertedCoachNames) {
+            Coach coach = new Coach(convertedCoachName);
+            coaches.add(coach);
+        }
+        return coaches;
+    }
+
+    public HateMenu saveHateMenus(List<String> convertedHateMenus) {
+        return new HateMenu(convertedHateMenus);
+    }
+
+    public Coaches saveCoachesInfo(List<Coach> validCoaches, List<HateMenu> validHateMenus) {
+        Coaches coaches = new Coaches();
+        for (int i = 0; i < validCoaches.size(); i++) {
+            coaches.addCoach(validCoaches.get(i), validHateMenus.get(i));
+        }
+        return coaches;
+    }
+}
