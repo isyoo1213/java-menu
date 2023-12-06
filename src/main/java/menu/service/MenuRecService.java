bbab5fc0de3ed27803129704d62d0eef4a6ae941
isyoo1213 <moethns@gmail.com>
2023-12-06 17:45:38 +0900

    feat: MenuRecService 메뉴 선정 기능 구현 service/MenuRecService - selectMenus()   - selectMenu() docs/README.md - 기능 명세 구현 체크

--- src/main/java/menu/service/MenuRecService.java
+++ src/main/java/menu/service/MenuRecService.java
@@ -1,12 +1,15 @@
 package menu.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import menu.constants.MenuConstants;
 import menu.model.Coach;
 import menu.model.Coaches;
 import menu.model.HateMenu;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MenuRecService {
 
@@ -33,5 +36,26 @@ public class MenuRecService {
 
     public MenuConstants selectCategory(int categoryNumber) {
         return MenuConstants.getCategoryByNumber(categoryNumber);
+    }
+
+    public Map<Coach, String> selectMenus(Coaches coaches, MenuConstants selectedCategory) {
+        Map<Coach, String> menuResult = new LinkedHashMap<>();
+        for (Map.Entry<Coach, HateMenu> coachInfo : coaches.provideCoachesInfo().entrySet()) {
+            String selectedMenu = selectMenu(selectedCategory, coachInfo);
+            menuResult.put(coachInfo.getKey(), selectedMenu);
+        }
+        return menuResult;
+    }
+
+    private String selectMenu(MenuConstants selectedCategory, Map.Entry<Coach, HateMenu> coachInfo) {
+        String selectedMenu = null;
+        boolean continueRec = true;
+        while (continueRec) {
+            selectedMenu = Randoms.shuffle(selectedCategory.getMenus()).get(0);
+            if (!coachInfo.getValue().hasMenu(selectedMenu)) {
+                continueRec = false;
+            }
+        }
+        return selectedMenu;
     }
 }
